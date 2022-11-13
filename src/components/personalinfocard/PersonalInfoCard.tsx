5063c5d04745fe0b9ca5c89a20673d1a758b279b
Bendt Schulz <bendt@schulz-hamburg.de>
2022-11-13 19:45:31 +0100

    added profi info layout

--- src/components/personalinfocard/PersonalInfoCard.tsx
+++ src/components/personalinfocard/PersonalInfoCard.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import * as styles from './PersonalInfoCard.module.css';
+import ProfilePic from '../../images/Bild.jpg';
+
 const PersonalInfoCard: React.FC = () => {
     return (
-        <div></div>
+        <div className={styles.infocard}>
+            <div className={styles.gridWrapper}>
+                <div className={`${styles.profilePic} ${styles.leftColumn}`}>
+                    <img className={styles.img} src={ProfilePic} />
+                </div>
+                <div className={styles.rightColum}>
+
+                </div>
+            </div>
+        </div>
     );
 };
 
