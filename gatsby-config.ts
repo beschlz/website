087cb1004713c74a33e8cda119c13d88c3e251e0
Bendt Schulz <bendt@schulz-hamburg.de>
2022-11-13 17:43:46 +0100

    removed inital gatsby stuff

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,13 +8,7 @@ const config: GatsbyConfig = {
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  graphqlTypegen: false,
 };
 
 export default config;
