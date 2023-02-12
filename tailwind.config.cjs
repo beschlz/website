a3eeae9475c7e8ecbf7598df6bec338a770b3d4c
Bendt Schulz <bendt@schulz-hamburg.de>
2023-02-12 11:51:16 +0100

    migrated EventCard to astro and tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,9 @@ module.exports = {
 			mainColor: "#0e1d33",
 			translucentMain: "rgba(0, 0, 0, 0.49)",
 			secondaryColor: "#b03800",
-			translucentWhite: "rgba(255, 255, 255, 0.3)"
+			translucentWhite: "rgba(255, 255, 255, 0.3)",
+			white: "rgb(255 255 255)",
+			black: "rgb(0 0 0)",
 		},
 		fontFamily: {
 			mono: ["FragmentMono"]
