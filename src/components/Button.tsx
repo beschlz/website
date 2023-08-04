d4dad69e4da8331a4560c7fe6971f82da84322b6
Bendt Schulz <bendt@schulz-hamburg.de>
2023-08-04 13:37:27 +0200

    feat: removed blog

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-type ButtonProps = {
-    text: string
-};
-
-const Button: React.FC<ButtonProps> = ({ text }) => {
-    return <button className="font-mono text-sm font-bold bg-secondaryColor pl-3 pr-3 p-2 rounded-md">{text}</button>
-};
-
-export default Button;
